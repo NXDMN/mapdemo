37927839f4f08fe661cd2a494963399b512329a4
NXDMN <norvintsl@gmail.com>
2024-09-20 13:54:00 +0800

    Add FlutterCompass iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,11 +11,15 @@ import GoogleMaps
       GMSServices.provideAPIKey("API_KEY")
       
       GeneratedPluginRegistrant.register(with: self)
+      
       guard let pluginRegistrar = self.registrar(forPlugin: "plugin-name") else { return false }
       let factory = FlutterStreetViewFactory(messenger: pluginRegistrar.messenger())
       pluginRegistrar.register(
           factory,
           withId: "flutter-street-view")
+      
+      let eventChannel = FlutterEventChannel(name: "flutter_compass", binaryMessenger: pluginRegistrar.messenger());
+      eventChannel.setStreamHandler(FlutterCompass())
       
       return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
